e7f35dfb418ec25c017340fd49df34afb43ed23d
Tiago100h <tiagogiuliatte.p@gmail.com>
2020-07-29 00:06:48 -0300

    Aula 2

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/Logo.png';
-import './Menu.css';
+import { LogoImage, MenuWrapper } from './style';
 import Button from '../Button';
-// import ButtonLink from './components/ButtonLink';
+import './Menu.css';
 
 function Menu() {
     return (
-        <nav className="Menu">
-            <a href="/">
-                <img className="Logo" src={Logo} alt="TiagoFlix logo" />
-            </a>
+        <MenuWrapper className="Menu">
+            <Link to="/">
+                <LogoImage src={Logo} alt="TiagoFlix logo" />
+            </Link>
 
-            <Button as="a" className="ButtonLink" href="/">
+            <Button as={Link} to="/cadastro/video">
                 Novo vídeo
             </Button>
-        </nav>
+        </MenuWrapper>
     );
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import Menu from './components/Menu'
-import dadosIniciais from './data/dados_iniciais.json';
-import BannerMain from './components/BannerMain';
-import Carousel from './components/Carousel';
-import Footer from './components/Footer';
+import styled from 'styled-components';
 
-function App() {
+import Menu from '../../components/Menu'
+import dadosIniciais from '../../data/dados_iniciais.json';
+import BannerMain from '../../components/BannerMain';
+import Carousel from '../../components/Carousel';
+import Footer from '../../components/Footer';
+
+const HomeWrapper = styled.div`
+  background: var(--grayDark);
+`;
+
+function Home() {
   return (
-    <div style={{ background: "#141414" }}>
+    <HomeWrapper>
       <Menu />
 
       <BannerMain
@@ -42,8 +48,8 @@ function App() {
       />      
 
       <Footer />
-    </div>
+    </HomeWrapper>
   );
 }
 
-export default App;
+export default Home;
